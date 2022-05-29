cb6cc25463ca148d0ffb5348dc38731f5d030495
canbe <824737880@qq.com>
2022-05-30 07:21:03 +0800

    OA_SSM模版构建成功

---
+++ src/main/java/com/cxd/controller/BookController.java
@@ -0,0 +1,79 @@
+package com.cxd.controller;
+
+import java.util.List;
+
+import com.cxd.entity.Book;
+import com.cxd.service.BookService;
+import com.cxd.service.impl.BookServiceImpl;
+import com.cxd.dto.AppointExecution;
+import com.cxd.dto.Result;
+import com.cxd.enums.AppointStateEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cxd.exception.NoNumberException;
+import com.cxd.exception.RepeatAppointException;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
+public class BookController {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Resource
+//	@Qualifier("BookServiceImpl")
+	private BookService bookService =new BookServiceImpl();
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	private String list(Model model) {
+		List<Book> list = bookService.getList();
+		model.addAttribute("list", list);
+		// list.jsp + model = ModelAndView
+		return "allbook";// WEB-INF/jsp/"list".jsp
+	}
+
+	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+	private String detail(@PathVariable("bookId") Long bookId, Model model) {
+		if (bookId == null) {
+			return "redirect:/book/list";
+		}
+		Book book = bookService.getById(bookId);
+		if (book == null) {
+			return "forward:/book/list";
+		}
+		model.addAttribute("book", book);
+		return "detail";
+	}
+
+	// ajax json
+	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
+			"application/json; charset=utf-8" })
+	@ResponseBody
+	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
+		if (studentId == null || studentId.equals("")) {
+			return new Result<>(false, "学号不能为空");
+		}
+		AppointExecution execution = null;
+		try {
+			execution = bookService.appoint(bookId, studentId);
+		} catch (NoNumberException e1) {
+			execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
+		} catch (RepeatAppointException e2) {
+			execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
+		} catch (Exception e) {
+			execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
+		}
+		return new Result<AppointExecution>(true, execution);
+	}
+
+}
